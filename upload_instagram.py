602bf702b4bad260f9cd82699b224b752204d7b7
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-01-30 09:30:07 +1100

    Change function arguments

--- upload_instagram.py
+++ upload_instagram.py
@@ -8,24 +8,24 @@ INSTAGRAM_LOGIN = os.getenv('INSTAGRAM_LOGIN')
 INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
 
 
-def thumbnail_pic(upload_folder):
-    download_folder = 'images'
-    images = os.listdir(download_folder)
+def thumbnail_pic(end_folder):
+    start_folder = 'images'
+    images = os.listdir(start_folder)
     for image in images:
-        start_file_path = os.path.join(os.getcwd(), download_folder)
+        start_file_path = os.path.join(os.getcwd(), start_folder)
         photo = Image.open(os.path.join(start_file_path, image))
         photo.thumbnail((1080, 1080))
         rgb_photo = photo.convert('RGB')
-        end_file_path = os.path.join(os.getcwd(), upload_folder, image)
+        end_file_path = os.path.join(os.getcwd(), end_folder, image)
         rgb_photo.save(f'{end_file_path}', format='JPEG')
 
 
-def upload_instagram(upload_folder):
+def upload_instagram(end_folder):
     bot = Bot()
     bot.login(username=INSTAGRAM_LOGIN, password=INSTAGRAM_PASSWORD)
-    images_for_instagram = os.listdir(upload_folder)
+    images_for_instagram = os.listdir(end_folder)
     for pic in images_for_instagram:
-        bot.upload_photo(os.path.join(os.getcwd(), upload_folder, pic))
+        bot.upload_photo(os.path.join(os.getcwd(), end_folder, pic))
 
 
 def main():
